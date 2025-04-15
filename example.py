ed567b86581b14e49cdd4f9c151d38ea1dceab43
Francois Pachet <pachet@gmail.com>
2025-04-15 18:47:18 +0200

    fixed the example.py

--- example.py
+++ example.py
@@ -1,14 +1,12 @@
-from ctor import Continuator2
+from ctor.continuator import Continuator2
 
 # Initialize the model
-midi_file_path = "../../data/prelude_c.mid"
+midi_file_path = "data/prelude_c.mid"
 generator = Continuator2(midi_file_path, 4, transposition=False)
 
 # set positional constraints
-constraints = {}
+constraints = {0: generator.get_vp_for_pitch(62), 19: generator.get_end_vp()}
 # constraints[0] = generator.get_start_vp()
-constraints[0] = generator.get_vp_for_pitch(62)
-constraints[19] = generator.get_end_vp()
 
 #generate the viewpoint sequence:
 generated_sequence = generator.sample_sequence(length=20, constraints=constraints)
@@ -20,4 +18,4 @@ sequence_to_render = generated_sequence[0:-1]
 rendered_sequence = generator.realize_vp_sequence(sequence_to_render)
 
 # save the generated sequence
-generator.save_midi(rendered_sequence, "../../data/ctor2_output.mid", tempo=-1)
+generator.save_midi(rendered_sequence, "data/ctor2_output.mid", tempo=-1)
