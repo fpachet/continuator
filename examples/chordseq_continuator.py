35c71a8b6d90c5dce86479816a1a35565540e99e
Francois Pachet <pachet@gmail.com>
2025-04-16 14:50:53 +0200

    fixed chord sequence example

--- examples/chordseq_continuator.py
+++ examples/chordseq_continuator.py
@@ -4,12 +4,18 @@ from pathlib import Path
 from ctor.variable_order_markov import Variable_order_Markov
 
 if __name__ == '__main__':
-    content = Path("../data/chord_sequences.txt").read_text()
-    content = content.replace("\n", ";")
-    chord_labels = [label.strip() for label in content.split(";")]
-    vo = Variable_order_Markov(chord_labels, None, 3)
-    length = 10
-    seq = vo.sample_sequence(length, constraints={0: vo.get_viewpoint('C'), length - 1: vo.get_viewpoint('C')})
-    result = ' '.join(seq)
-    result = re.sub(r"\s([?.!,:;”])", r"\1", result)
-    print(result)
+    # computes chord sequences starting and ending with, say, C
+    with open('../data/chord_sequences.txt', 'r') as file:
+        seqs = file.readlines()
+    seqs = [seq.split(';')[1:-1] for seq in seqs]
+    seqs = [[chord.strip() for chord in seq] for seq in seqs]
+    vo = Variable_order_Markov(None, None, kmax=3)
+    for seq in seqs:
+        vo.learn_sequence(seq)
+
+    for i in range(20):
+        length = 10
+        seq = vo.sample_sequence(length, constraints={0: vo.get_viewpoint('C'), length - 1: vo.get_viewpoint('C')})
+        result = ' '.join(seq)
+        result = re.sub(r"\s([?.!,:;”])", r"\1", result)
+        print(result)
