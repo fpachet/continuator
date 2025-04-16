494e50d7f0e5807e589600216d3b9faff44407d7
Francois Pachet <pachet@gmail.com>
2025-04-16 12:49:47 +0200

    added chord sequence example

--- examples/chordseq_continuator.py
+++ examples/chordseq_continuator.py
@@ -9,7 +9,7 @@ if __name__ == '__main__':
     content = content.replace("\n", ";")
     chord_labels = [label.strip() for label in content.split(";")]
     vo = Variable_order_Markov(chord_labels, None, 3)
-    length = 20
+    length = 10
     seq = vo.sample_sequence(length, constraints={0: vo.get_viewpoint('C'), length-1: vo.get_viewpoint('C')})
     result = ' '.join(seq)
     result = re.sub(r"\s([?.!,:;”])", r"\1", result)
